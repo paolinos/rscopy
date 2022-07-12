59917c7b48ed5b71370a62adb60ecc4545ea6944
Pablo C <pendlejin@yahoo.com.ar>
2022-07-12 06:55:55 +0000

    add new app usecase

--- Readme.md
+++ Readme.md
@@ -8,13 +8,13 @@ If you are trying to use this, please DON'T!
 Only copy files with absolute path
 ```
 // To run 
-cargo run {source_file} {destination_file}
+cargo run {source_file} {destination_folder}
 
 // Copy file with limit of 50Megas
-cargo run {source_file} {destination_file} -b 50 
+cargo run {source_file} {destination_folder} -b 50 
 
 // NOTE: Not implemente yet
-cargo run {source_file} {destination} -o {new_name}
+cargo run {source_file} {destination_folder} -o {new_filename}
 ```
 
 # TODO:
@@ -26,3 +26,24 @@ cargo run {source_file} {destination} -o {new_name}
 - check permission before copy
 - add async or parallel (or both async could be to copy bytes and parallel to run N times using CPU)?
 - improve terminal feedback displaying file name and percentage to copy
+
+
+
+### Build
+
+**build windows app in linux**
+```bash
+# install dependency
+apt-get install mingw-w64
+# build 
+cargo build --target x86_64-pc-windows-gnu
+```
+
+**examples**
+```bash
+# copy in windows
+.\copy_file.exe "C:\some_image.jpg" "C:\temp\image.jpg"
+
+# copy in linux
+cargo run /tmp/image.jpg /user/tmp/destination
+```
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,48 @@
 // Remember to include the mod and then use it
 mod tools;
-mod copy;
-use tools::argument::Argument;
+//mod copy;
+mod common;
+mod usecases;
+mod consts;
 
-use tools::file::FileHelper;
+use tools::argument::Argument;
 
 // TODO: find a way to put all const in one file. so far we can call this with crate::MEGABYTE
 pub const MEGABYTE: u32 = 1024000;
 
 fn main() {
-    let args = Argument::get();    
+    let args = Argument::get();
     // Debug
     //dbg!(&args);
 
+    // TODO:
+    //  TODO: get required parameters
+    //  TODO: call usecase to copy with options
+    //  TODO: UseCase -> check if source exist && is file
+    //  TODO: UseCase -> check if destination exist && is file, if exist clean file.
+    //  TODO: UseCase -> copy file
+
+
+    println!("Starting program");
+
+    let mut errors = String::from("");
+
     if args.is_some() {
         let data = args.unwrap();
-        
-        // TODO: this should receive the args
-        match FileHelper::copy(data.source(), data.destination(), data.byte_limit()){
+
+        match usecases::app::copy_from_to(data){
             Ok(()) => println!("Ok"),
-            Err(e) => println!("Error trying to read the message: {}", e)
-        };
-    }else{
+            Err(e) => {
+                println!("Error trying to read the message: {}", e);
+                errors = e.to_string();
+            }
+        }
+    }
+    else {
+        errors = "Missing parameters".to_string();
+    }
+    
+    if !String::is_empty(&errors){
         // TODO: add more information about what logic is need
         // TODO: implement --help should be nice
         println!("Nothing to copy");
